6100061ffaad5060f93e8227c8f11cca929ef81a
ranjitha1213 <ranjithahn31265@gmail.com>
2025-05-15 16:26:14 -0500

    Demo.java updated successfully

--- Demo.java
+++ Demo.java
@@ -1,6 +1,12 @@
 public class Demo{
+	public void add( ){
+		int a=10,b=10;
+		int c=a+b;
+		System.out.println("Sum="+c);
+		
+	}
 	public static void main(String args[]){
-              String s="hello chanadana"
-              System.out.println(s)
+              String s="hello chanadana";
+              System.out.println(s);
 }
 }
